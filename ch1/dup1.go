5d0d10d9b4da20c3e0b5acf29c36c704ee41e724
Peter Admilson Ramaldes <peter.ramaldes@gmail.com>
2023-02-03 06:37:56 -0300

    Added dup1.go

---
+++ ch1/dup1.go
@@ -0,0 +1,24 @@
+// Dup1 show the text for the entire line that appear more than once in the
+// stdin, preceded by the count
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	input := bufio.NewScanner(os.Stdin)
+
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
