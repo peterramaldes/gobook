097d3424c3a95c32d94a59ab82385eb104af7bba
Peter Admilson Ramaldes <peter.ramaldes@gmail.com>
2023-02-05 07:59:07 -0300

    Added fetchall

---
+++ ch1/fetchall.go
@@ -0,0 +1,42 @@
+// FetchAll show the content in which url specified concurrent
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	ch := make(chan string)
+	for _, url := range os.Args[1:] {
+		go fetch(url, ch) // initiate goroutine
+	}
+	for range os.Args[1:] {
+		fmt.Println(<-ch) // receive channel ch
+	}
+	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+}
+
+func fetch(url string, ch chan<- string) {
+	start := time.Now()
+	resp, err := http.Get(url)
+	if err != nil {
+		ch <- fmt.Sprint(err) // send err to channel ch
+		return
+	}
+
+	nbytes, err := io.Copy(ioutil.Discard, resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		ch <- fmt.Sprintf("while reading %s: %v", url, err)
+		return
+	}
+
+	secs := time.Since(start).Seconds()
+	ch <- fmt.Sprintf("%.2fs %7d %s", secs, nbytes, url)
+}
