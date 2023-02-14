f19179893f0e85b0ac41ef1655cc4b8ad55c4e7d
Peter Admilson Ramaldes <peter.ramaldes@gmail.com>
2023-02-14 05:11:14 -0300

    Added Clock

---
+++ ch8/clock2/main.go
@@ -0,0 +1,36 @@
+// Clock2 is a TCP server responsible for write time periodically
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+	"time"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		go handleConn(conn)
+	}
+}
+
+func handleConn(c net.Conn) {
+	defer c.Close()
+	for {
+		_, err := io.WriteString(c, time.Now().Format(time.RFC1123+"\n"))
+		if err != nil {
+			return
+		}
+		time.Sleep(time.Second * 1)
+	}
+}
